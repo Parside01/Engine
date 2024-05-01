a8ea9444d1b4c8c9b062bfdf218c6d9914d60e42
parside <vadimakovlev79449@gmail.com>
2024-05-01 22:35:40 +0800

    ebke

---
+++ Engine/src/app/Application.cpp
@@ -0,0 +1,15 @@
+#include "Application.hpp"
+
+
+namespace Engine
+{
+    Application::Application() {
+
+    }
+    Application::~Application() {
+
+    }
+    void Application::Run() {
+        
+    }
+} // namespace Engine
