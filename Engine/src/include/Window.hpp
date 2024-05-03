596b835eae28065c6ba80dc4f517d4bf25bef257
Kneepy <kneepydev@gmail.com>
2024-05-03 20:24:55 +0800

    keb

---
+++ Engine/src/include/Window.hpp
@@ -0,0 +1,41 @@
+#ifndef ENGINE_WINDOWN_HPP
+#define ENGINE_WINDOWN_HPP
+
+#include "engine_precompile_headers.hpp"
+
+#include "Core.hpp"
+#include "events/Event.hpp"
+
+namespace Engine
+{   
+    class WindowProrepty {
+    private:
+        std::string Title;
+        uint32_t Width;
+        uint32_t Height;
+
+        WindowProrepty(const std::string& title = "Engine", uint32_t width = 1280, uint32_t height = 720)
+            : Title(title), Width(width), Height(height) {}
+    };
+
+    class ENGINE_API Window {
+    public:
+        typedef std::function<void(Event&)> EventCallbackFn;
+
+        virtual ~Window() {}
+
+        virtual void OnUpdate() = 0;
+
+        virtual uint32_t GetWidth() const = 0;
+        virtual uint32_t GetHeight() const = 0;
+
+        virtual void SetEventCallback(const EventCallbackFn& callback) = 0;
+
+        static Window* Create(const WindowProrepty& props = WindowProrepty());
+
+    };
+} // namespace Engine
+
+
+
+#endif
