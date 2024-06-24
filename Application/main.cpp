fba9ceca2cb595266602acc88cb7396c86e78b2d
Parside01 <vadimakovlev79449@gmail.com>
2024-06-24 21:32:01 +0800

    Теперь стандратная сцена - кубик, который можно вертеть.

--- Application/main.cpp
+++ Application/main.cpp
@@ -5,14 +5,19 @@
 class TestLayer : public Engine::Layer {
 public:
     TestLayer()
-        : Layer("Test"), m_Camera(-1.6f, 1.6f, -0.9f, 0.9f), m_CameraPosition(0.f), m_SquarePosition(0.f)
+        : Layer("Test"), m_Camera(-1.6f, 1.6f, -0.9f, 0.9f), m_CameraPosition(0.f), m_CubePosition(0.f)
     {
         m_VertexArray.reset(Engine::VertexArray::Create());
 
-        float vertices[3 * 7] = {
-            -0.5f, -0.5f, 0.0f, 0.8f, 0.2f, 0.8f, 1.f,
-             0.5f, -0.5f, 0.0f, 0.2f, 0.3f, 0.8f, 1.f,
-             0.0f,  0.5f, 0.0f, 0.8f, 0.8f, 0.2f, 1.f,
+        float vertices[3 * 8] = {
+            -0.5f, -0.5f, -0.5f,
+             0.5f, -0.5f, -0.5f,
+             0.5f,  0.5f, -0.5f,
+            -0.5f,  0.5f, -0.5f,
+            -0.5f, -0.5f,  0.5f,
+             0.5f, -0.5f,  0.5f,
+             0.5f,  0.5f,  0.5f,
+            -0.5f,  0.5f,  0.5f,
         };
 
         std::shared_ptr<Engine::VertexBuffer> vertexBuffer;
@@ -21,58 +26,32 @@ public:
         {
             Engine::BufferLayout layout = {
                 {"a_Position", Engine::ShaderDataType::Float3},
-                {"a_Color", Engine::ShaderDataType::Float4},
             };
             vertexBuffer->SetLayout(layout);
         }
 
         m_VertexArray->AddVertexBuffer(vertexBuffer);
 
-        uint32_t indices[3] = { 0, 1, 2 };
+        uint32_t indices[36] = {
+            0, 1, 2, 2, 3, 0,
+            4, 5, 6, 6, 7, 4,
+            0, 4, 5, 5, 1, 0,
+            1, 5, 6, 6, 2, 1,
+            2, 6, 7, 7, 3, 2,
+            3, 7, 4, 4, 0, 3,
+        };
         std::shared_ptr<Engine::IndexBuffer> indexBuffer;
         indexBuffer.reset(Engine::IndexBuffer::Create(indices, sizeof(indices) / sizeof(uint32_t)));
         m_VertexArray->SetIndexBuffer(indexBuffer);
 
-        //// TESTING ////
-
-        m_SquareVA.reset(Engine::VertexArray::Create());
-
-        float blue_vertices[3 * 4] = {
-            -0.75f, -0.75f, 0.70f,
-             0.75f, -0.75f, 0.70f,
-             0.75f,  0.75f, 0.70f,
-            -0.75f,  0.75f, 0.70f,
-        };
-
-        std::shared_ptr<Engine::VertexBuffer> squareVB;
-        squareVB.reset(Engine::VertexBuffer::Create(blue_vertices, sizeof(blue_vertices)));
-        {
-            squareVB->SetLayout({
-                {"a_Position", Engine::ShaderDataType::Float3},
-            });
-        }
-        m_SquareVA->AddVertexBuffer(squareVB);
-
-        uint32_t blue_indices[6] = { 0, 1, 2, 2, 3, 0 };
-        std::shared_ptr<Engine::IndexBuffer> squareIB;
-        squareIB.reset(Engine::IndexBuffer::Create(blue_indices, sizeof(blue_indices) / sizeof(uint32_t)));
-        m_SquareVA->SetIndexBuffer(squareIB);
-
-        //// TESTING ////
-
         std::string vertexSrc = R"(
             #version 330 core
             layout(location = 0) in vec3 a_Position;
-            layout(location = 1) in vec4 a_Color;
 
             uniform mat4 u_ViewProjection;
             uniform mat4 u_Transform;
 
-            out vec3 v_Position;
-            out vec4 v_Color;
             void main() {
-                v_Position = a_Position;
-                v_Color = a_Color;
                 gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);
             }
         )";
@@ -80,57 +59,34 @@ public:
         std::string fragmentSrc = R"(
             #version 330 core
             layout(location = 0) out vec4 color;
-            in vec3 v_Position;
-            in vec4 v_Color;
             void main() {
-                color = vec4(v_Position * 0.5 + 0.5, 1.0);
-                color = v_Color;
+                color = vec4(1.0, 0.5, 0.2, 1.0);
             }
         )";
 
         m_Shader = std::make_unique<Engine::Shader>(vertexSrc, fragmentSrc);
-
-
-        //// TESTING ////
-        std::string blue_vertexSrc = R"(
-            #version 330 core
-            layout(location = 0) in vec3 a_Position;
-
-            uniform mat4 u_ViewProjection;
-            uniform mat4 u_Transform;
-
-            out vec3 v_Position;
-
-            void main() {
-                v_Position = a_Position;
-                gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);
-            }
-        )";
-
-        std::string blue_fragmentSrc = R"(
-            #version 330 core
-            layout(location = 0) out vec4 color;
-            in vec3 v_Position;
-            void main() {
-                color = vec4(0.2, 0.3, 0.8, 0.2);
-            }
-        )";
-
-        blue_m_Shader.reset(new Engine::Shader(blue_vertexSrc, blue_fragmentSrc));
-
-        //// TESTING ////
     }
     void OnUpdate(Engine::Timestep tick) override {
-        if (Engine::Input::IsKeyPressed(EG_KEY_LEFT))
+        if (Engine::Input::IsKeyPressed(EG_KEY_W))
+            m_CubeRotation.x += m_CubeRotationSpeed * tick;
+        else if (Engine::Input::IsKeyPressed(EG_KEY_S))
+            m_CubeRotation.x -= m_CubeRotationSpeed * tick;
+
+        if (Engine::Input::IsKeyPressed(EG_KEY_A))
+            m_CubeRotation.y -= m_CubeRotationSpeed * tick;
+        else if (Engine::Input::IsKeyPressed(EG_KEY_D))
+            m_CubeRotation.y += m_CubeRotationSpeed * tick;
+
+        if (Engine::Input::IsKeyPressed(EG_KEY_RIGHT))
             m_CameraPosition.x += m_CameraSpeed * tick;
-        else if (Engine::Input::IsKeyPressed(EG_KEY_RIGHT))
+        else if (Engine::Input::IsKeyPressed(EG_KEY_LEFT))
             m_CameraPosition.x -= m_CameraSpeed * tick;
 
         if (Engine::Input::IsKeyPressed(EG_KEY_UP))
-            m_CameraPosition.y -= m_CameraSpeed * tick;
-        else if (Engine::Input::IsKeyPressed(EG_KEY_DOWN))
             m_CameraPosition.y += m_CameraSpeed * tick;
-        
+        else if (Engine::Input::IsKeyPressed(EG_KEY_DOWN))
+            m_CameraPosition.y -= m_CameraSpeed * tick;
+
 
         Engine::RenderCommand::SetClearColor({0.1f, 0.1f, 0.1f, 1});
         Engine::RenderCommand::Clear();
@@ -139,10 +95,10 @@ public:
 
         Engine::Renderer::BeginScene(m_Camera);
         {
-            glm::mat4 transform = glm::translate(glm::mat4(1.f), m_SquarePosition);
-
-            Engine::Renderer::Submit(m_SquareVA, blue_m_Shader, transform);
-            Engine::Renderer::Submit(m_VertexArray, m_Shader);
+            glm::mat4 transfrom = glm::translate(glm::mat4(1.f), m_CubePosition);
+            transfrom = glm::rotate(transfrom, glm::radians(m_CubeRotation.x), glm::vec3(1.f, 0.f, 0.f));
+            transfrom = glm::rotate(transfrom, glm::radians(m_CubeRotation.y), glm::vec3(0.f, 1.f, 0.f));
+            Engine::Renderer::Submit(m_VertexArray, m_Shader, transfrom);
         }
         Engine::Renderer::EndScene();
 
@@ -166,15 +122,13 @@ private:
     std::shared_ptr<Engine::VertexArray> m_VertexArray;
     std::shared_ptr<Engine::Shader> m_Shader;
 
-    std::shared_ptr<Engine::VertexArray> m_SquareVA;
-    std::shared_ptr<Engine::Shader> blue_m_Shader;
-
     Engine::OrthCamera m_Camera;
     glm::vec3 m_CameraPosition;
-    float m_CameraSpeed{1.f};
+    float m_CameraSpeed{2.f};
 
-    glm::vec3 m_SquarePosition;
-    float m_SquareSpeed{5.f};
+    glm::vec3 m_CubePosition;
+    glm::vec2 m_CubeRotation;
+    float m_CubeRotationSpeed{20.f};
 };
 
 class Sandbox : public Engine::Application {
--- Engine/src/Render/OrthCamera.cpp
+++ Engine/src/Render/OrthCamera.cpp
@@ -6,7 +6,7 @@
 namespace Engine
 {
     OrthCamera::OrthCamera(float left, float right, float bottom, float top)
-        : m_ProjectionMatrix(glm::ortho(left, right, bottom, top, -1.0f, 1.0f)), m_ViewMatrix(1.0f)
+        : m_ProjectionMatrix(glm::ortho(left, right, bottom, top, -9.0f, 9.0f)), m_ViewMatrix(1.0f)
     {
         m_ViewProjectionMatrix = m_ProjectionMatrix * m_ViewMatrix;
     }
