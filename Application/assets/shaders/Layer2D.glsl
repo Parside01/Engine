86a76cd45fe1f74331df0f2ea27fc06cdc5cfaa7
Kneepy <kneepydev@gmail.com>
2024-07-19 21:57:42 +0800

    Поставил версию glsl 410 такак на компе wsl не поддерживает 460

--- Application/assets/shaders/Layer2D.glsl
+++ Application/assets/shaders/Layer2D.glsl
@@ -1,5 +1,5 @@
 #type vertex
-#version 460 core
+#version 410 core
 
 layout(location = 0) in vec3 a_Position;
 layout(location = 1) in vec4 a_Color;
@@ -21,7 +21,7 @@ void main() {
 }
 
 #type fragment
-#version 460 core
+#version 410 core
 
 layout(location = 0) out vec4 color;
 
