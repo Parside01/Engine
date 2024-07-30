12817de55c8c14e752baf3ceb35be28324ad2002
Parside01 <vadimakovlev79449@gmail.com>
2024-07-30 20:32:53 +0800

    Решил проблему с загрузкой intов в layout

--- Engine/src/Render/OpenGL/VertexArray_OpenGL.cpp
+++ Engine/src/Render/OpenGL/VertexArray_OpenGL.cpp
@@ -20,7 +20,7 @@ namespace Engine
             case ShaderDataType::Bool:   return GL_BOOL;
             default: return 0;
         }
-        EG_CORE_ASSERT(false, "Unknow ShaderDataType");
+        EG_CORE_ASSERT(false, "ShaderDataTypeToOpenGLBaseType - Unknow ShaderDataType");
         return 0;
     }
     #define TOSTRING(type) #type
@@ -47,21 +47,65 @@ namespace Engine
 
         EG_CORE_ASSERT(vertexBuffer->GetLayout().GetElements().size(), "Vertex Buffer has no layout!");
 
-        uint32_t index{0};
         const BufferLayout& layout = vertexBuffer->GetLayout();
         for (const auto& i : layout)
-        {
-            if (i.Name == "a_EntityID" && i.Type == ShaderDataType::Int) EG_CORE_WARN("KMBKREMBKMBK"); 
-            glEnableVertexAttribArray(index);
-            glVertexAttribPointer(
-                index,
-                i.GetComponentCount(),
-                ShaderDataTypeToOpenGLBaseType(i.Type),
-                i.Normalized ? GL_TRUE : GL_FALSE,
-                layout.GetStride(),
-                reinterpret_cast<const void*>(i.Offset)
-            );
-            ++index;
+        {   
+            switch (i.Type)
+            {
+                case ShaderDataType::Float: 
+                case ShaderDataType::Float2: 
+                case ShaderDataType::Float3: 
+                case ShaderDataType::Float4: {
+                    glEnableVertexAttribArray(m_VertexBufferIndex);
+                    glVertexAttribPointer(
+                        m_VertexBufferIndex,
+                        i.GetComponentCount(),
+                        ShaderDataTypeToOpenGLBaseType(i.Type),
+                        i.Normalized ? GL_TRUE : GL_FALSE,
+                        layout.GetStride(),
+                        reinterpret_cast<const void*>(i.Offset)
+                    );
+                    m_VertexBufferIndex++;
+                    break;
+                }
+                case ShaderDataType::Int: 
+                case ShaderDataType::Int2: 
+                case ShaderDataType::Int3: 
+                case ShaderDataType::Int4: 
+                case ShaderDataType::Bool: {
+                    glEnableVertexAttribArray(m_VertexBufferIndex);
+                    glVertexAttribIPointer(
+                        m_VertexBufferIndex,
+                        i.GetComponentCount(),
+                        ShaderDataTypeToOpenGLBaseType(i.Type),
+                        layout.GetStride(),
+                        reinterpret_cast<const void*>(i.Offset)
+                    );
+                    m_VertexBufferIndex++;
+                    break;
+                }
+                case ShaderDataType::Mat3:
+                case ShaderDataType::Mat4: {
+                    uint8_t count = i.GetComponentCount();
+                    for (uint8_t j{0}; j < count; ++j) {
+                        glEnableVertexAttribArray(m_VertexBufferIndex);
+
+                        glVertexAttribPointer(
+                            m_VertexBufferIndex,
+                            count,
+                            ShaderDataTypeToOpenGLBaseType(i.Type),
+                            i.Normalized ? GL_TRUE : GL_FALSE,
+                            layout.GetStride(),
+                            reinterpret_cast<const void*>(i.Offset + sizeof(float) * count * j)
+                        );
+                        m_VertexBufferIndex++;
+                    }
+                    break;
+                }   
+                std::cout << i.Name << std::endl; 
+                EG_CORE_ASSERT(false, "OpenGLVertexArray::AddVertexBuffer - Unknow ShaderDataType");
+                return;
+            }
         }
         m_VertexBuffers.push_back(vertexBuffer);
     }
